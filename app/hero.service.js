43feafc74ff08e5b8f35455962f79bd5a35ad5ef
Abhi2613 <jaimini2613abhishek@gmail.com>
2016-08-01 12:53:24 +0530

    My first GIT

---
+++ app/hero.service.js
@@ -0,0 +1,29 @@
+"use strict";
+var mock_heroes_1 = require('./mock-heroes');
+//@Injectable()
+var HeroService = (function () {
+    function HeroService(http) {
+        this.http = http;
+        this.heroesUrl = 'app/heroes'; // URL to web api
+    }
+    HeroService.prototype.getHeroes = function () {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.getHeroesSlowly = function () {
+        return new Promise(function (resolve) {
+            return setTimeout(function () { return resolve(mock_heroes_1.HEROES); }, 2000);
+        } // 2 seconds
+         // 2 seconds
+        );
+    };
+    HeroService.prototype.getHero = function (id) {
+        return this.getHeroes()
+            .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
+    };
+    return HeroService;
+}());
+exports.HeroService = HeroService;
+//# sourceMappingURL=hero.service.js.map
---
+++ app/hero.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+
+
+
+//@Injectable()
+
+export class HeroService {
+
+    private heroesUrl = 'app/heroes';  // URL to web api
+
+    constructor(private http: Http) { }
+    getHeroes() {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
+    }
+
+    getHeroesSlowly() {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+        );
+    }
+
+
+
+    getHero(id: number) {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
+}
+
+
+
